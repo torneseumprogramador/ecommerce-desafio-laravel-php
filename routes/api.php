0e3c21e021edfbd8a67f2ae31dd1d87bacbafefa
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-11-06 06:37:11 -0300

    API com laravel

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,12 @@ use App\Http\Controllers\ClientesController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::resource('clientes', ClientesController::class);
+// Route::resource('clientes', ClientesController::class);
+
+Route::get('/clientes', [ClientesController::class, 'index'])->name('clientes.index');
+Route::post('/clientes', [ClientesController::class, 'store'])->name('clientes.store');
+Route::get('/clientes/{cliente}', [ClientesController::class, 'show'])->name('clientes.show');
+Route::put('/clientes/{cliente}', [ClientesController::class, 'update'])->name('clientes.update');
+Route::patch('/clientes/{cliente}', [ClientesController::class, 'update']);
+Route::delete('/clientes/{cliente}', [ClientesController::class, 'destroy'])->name('clientes.destroy');
+
