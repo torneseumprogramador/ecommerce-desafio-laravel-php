f71c34fc46098358b1bffaefb557b47d3d8a9404
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-11-11 07:30:29 -0300

    respondendo rotas sem /api tb

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ClientesController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/clientes', [ClientesController::class, 'index'])->name('clientes.index');
+Route::post('/clientes', [ClientesController::class, 'store'])->name('clientes.store');
+Route::get('/clientes/{cliente}', [ClientesController::class, 'show'])->name('clientes.show');
+Route::put('/clientes/{cliente}', [ClientesController::class, 'update'])->name('clientes.update');
+Route::patch('/clientes/{cliente}', [ClientesController::class, 'update']);
+Route::delete('/clientes/{cliente}', [ClientesController::class, 'destroy'])->name('clientes.destroy');
+
